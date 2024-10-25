a8142721784cf50f9a6b05d459caa95d313c6785
Devasai Chinthakuntla <117557578+Chinthakuntla625@users.noreply.github.com>
2024-10-25 17:19:51 +0530

    Update Nike.tsx

--- src/Nike.tsx
+++ src/Nike.tsx
@@ -1,110 +1,140 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom'; 
-import { Card, CardMedia, CardContent, Typography, Grid, Button } from '@mui/material';
-import { styled } from '@mui/material/styles';
+
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Footer from './Footer';
-import { SiNike } from "react-icons/si";
+import { Card, CardContent, Typography, Grid, Container, Button, CircularProgress } from '@mui/material';
+import { styled } from '@mui/material/styles';
 
-const NikeCard = styled(Card)(({ theme }) => ({
-  maxWidth: 345,
+interface Item {
+  model: string;
+  price: number;
+  quantity: number;
+}
+
+interface Order {
+  date: string;
+  totalPrice: number;
+  items: Item[];
+  kicksID: string;
+}
+
+const ProductCard = styled(Card)(({ theme }) => ({
+  maxWidth: 1000,
   margin: theme.spacing(2),
-  boxShadow: theme.shadows[1],
-  transition: 'transform 0.2s ease-in-out',
-  '&:hover': {
-    transform: 'scale(1.03)',
-  }
 }));
 
-interface Product {
-  id: string;
-  image: string;
-  model: string;
-  brand: string;
-  price: number;
-  oldPrice?: number;
-}
+const Styledbtn=styled(Button)(()=>({
+  marginBottom: "16px", width: "200px",marginLeft:"4px" 
+  , marginTop: "16px",backgroundColor:"#f50057",color:"white",
+}))
 
-interface NikeProps {
-  searchQuery: string;
-}
+const OrderHistory: React.FC = () => {
+  const [orders, setOrders] = useState<Order[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+  const kicksID = sessionStorage.getItem("KicksID");
 
-const Nike: React.FC<NikeProps> = ({ searchQuery }) => {
-  const [products, setProducts] = useState<Product[]>([]);
-  const navigate = useNavigate(); 
+
+  // const handleDownload=()=>{
+  //   window.print();
+  // }
 
   useEffect(() => {
-    const fetchProducts = async () => {
+    const fetchOrders = async () => {
+      setLoading(true);
       try {
+        // const response = await axios.get<Order[]>('http://localhost:8000/Orderhistory');
         const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/db.json`);
-        const productsData = response.data.products; 
-        const nikeProducts =productsData.filter((product: { brand: string; }) => product.brand.toLowerCase() === 'nike');
-        setProducts(nikeProducts);
+        if (kicksID) {
+          const userOrders = response.data.Orderhistory.filter((order: { kicksID: string; }) => order.kicksID === kicksID);
+          setOrders(userOrders);
+        }
       } catch (error) {
-        console.log("Error:", error);
-      } 
+        setError("Error fetching orders. Please try again later.");
+      } finally {
+        setLoading(false);
+      }
     };
 
-    fetchProducts();
-  }, []);
+    fetchOrders();
+  }, [kicksID]);
 
-  const handleProductClick = (productId: string) => {
-    navigate(`/ProductDescription/${productId}`);
-  };
-
-  const filteredProducts = products.filter(product =>
-    product.model.toLowerCase().includes(searchQuery.toLowerCase())
-  );
+  if (loading) return <CircularProgress />;
 
   return (
     <div>
-      <Typography variant='h4'>
-        <b><SiNike style={{ margin: "12px" }} /> NIKE SNEAKERS</b>
-      </Typography>
-      <hr />
-      <Grid container spacing={4} justifyContent="center">
-        {filteredProducts.length === 0 ? (
-          <Typography>No products available</Typography>
+    <Container>
+      <div className="order-history">
+        {kicksID === null ? (
+          <Typography variant="h6" align="center" style={{ marginTop: "25px",height:"50vh" }}>
+            <b>PLEASE LOGIN TO VIEW YOUR ORDER HISTORY</b>
+          </Typography>
         ) : (
-          filteredProducts.map((product) => (
-            <Grid item xs={12} sm={6} md={4} lg={3} key={product.id}>
-              <NikeCard>
-                <CardMedia
-                  component="img"
-                  alt={product.model}
-                  height="185"
-                  image={product.image}
-                />
-                <CardContent>
-                  <Typography variant="body2" color="text.secondary">
-                    {product.brand}
-                  </Typography>
-                  <Typography variant="h6">
-                    <b>{product.model}</b>
-                  </Typography>
-                  <Typography variant="body2" color="text.primary" sx={{ display: "flex", flexDirection: "row" }}>
-                    <b style={{ color: "rgb(101,157,218)" }}>₹{product.price}</b>
-                    {product.oldPrice && (
-                      <Typography>
-                        <Typography style={{ fontWeight: "lighter", paddingLeft: "15px", fontSize: "small" }}>
-                          M.R.P.:<del style={{ fontWeight: "lighter", paddingLeft: "5px", fontSize: "small" }}>
-                          ₹{product.oldPrice}
-                        </del></Typography>
-                      </Typography>
-                    )}
-                  </Typography>
-                  <Button variant="contained" color="secondary" sx={{ mt: 2 }} onClick={() => handleProductClick(product.id)}>
-                    View Details
-                  </Button>
-                </CardContent>
-              </NikeCard>
-            </Grid>
-          ))
+          <>
+            <Typography variant='h4'  >
+              <b>ORDER HISTORY</b>
+            </Typography>
+            <hr></hr>
+            <Styledbtn variant='contained' color='secondary' >
+                <strong>USER NAME:</strong> <b style={{ color: "white" }}> {kicksID}</b>
+            </Styledbtn>
+          </>
+          
         )}
-      </Grid>
-      <Footer />
-    </div>
+       
+        {error && (
+          <Typography color="error" align="center" style={{ marginBottom: "16px" }}>
+            {error}
+          </Typography>
+        )}
+        <Grid container spacing={2}>
+          {orders.map((order, index) => (
+            <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+              <ProductCard>
+                <CardContent>
+                  <Typography variant="h6">
+                    <b>ORDER-{index + 1}</b>
+                  </Typography>
+                  <hr></hr>
+                  <Typography variant="body2">
+                    <b>Kicks ID:</b> {kicksID}
+                  </Typography>
+                  
+                  <Typography variant="body2">
+                    <b>Date:</b> {new Date(order.date).toLocaleString()}
+                  </Typography>
+                  <Typography variant="body2">
+                    <b>Total Price:</b> ₹{order.totalPrice.toFixed(2)}
+                  </Typography>
+                 
+                  <hr></hr>
+                  <ul style={{ listStyleType: "none", padding: 0 }}>
+                    {order.items.map((item, idx) => (
+                      <li key={idx}>
+                        <Typography variant="body2">
+                          <b>Item:</b> {item.model} - <b> ₹{item.price}</b>
+                        </Typography>
+                        <Typography variant="body2">
+                          <b>Quantity:</b> {item.quantity}
+                        </Typography>
+                      </li>
+                    ))}
+                  </ul>
+                {/* <Button variant='outlined' color='success' onClick={()=>handleDownload()}>Download</Button> */}
+                </CardContent>
+              </ProductCard>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+    </Container>
+     <Footer/>
+     </div>
   );
 };
 
-export default Nike;
+export default OrderHistory;
+
+
+
+
